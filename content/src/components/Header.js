918e1c00082db12eb33117c105319e4f8b6bdad5
Michael Chavez <machavez2011@gmail.com>
2018-06-03 02:48:52 -0700

    Made changes to the Header

--- content/src/components/Header.js
+++ content/src/components/Header.js
@@ -1,56 +1,28 @@
 import React from "react";
 import { Link, NavLink as RouterNavLink } from "react-router-dom";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from "reactstrap";
 import smallLogo from "./images/Logo.png";
 
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
+const logo = {
+  width: "25%"
+};
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
+export default class Example extends React.Component {
   render() {
     return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <a href="/">
-            <img src={smallLogo} href="/" />
-          </a>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink tag={Link} to="/create-group">
-                  Group
-                </NavLink>
-                <NavLink tag={Link} to="/rap-words">
-                  Rap Words
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <React.Fragment>
+        <div>
+          <img src={smallLogo} alt="tentIcon" style={logo} />
+          <Link to="/" className="btn btn-link">
+            Home
+          </Link>
+          <Link to="/create-group" className="btn btn-link">
+            Create Group
+          </Link>
+          <Link to="/rap-words" className="btn btn-link">
+            Rap Words
+          </Link>
+        </div>
+      </React.Fragment>
     );
   }
 }
