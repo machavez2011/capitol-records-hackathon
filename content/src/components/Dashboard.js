4208ff2e409d62b45f6b320856093023290c91e9
System Administrator <root@Hyun-Dos-MacBook-Pro.local>
2018-06-03 00:16:39 -0700

    commit before merge

--- content/src/components/Dashboard.js
+++ content/src/components/Dashboard.js
@@ -12,7 +12,11 @@ class Dashboard extends React.Component {
       <Grid>
         <Row>
           <Col>
-            <iframe src="https://tokbox.com/embed/embed/ot-embed.js?embedId=f6c1282f-9546-439c-aa4a-c74ca907164d&room=DEFAULT_ROOM&iframe=true" allow="microphone; camera" ></iframe>
+          <iframe
+          width="400" height="300"
+             src="https://tokbox.com/embed/embed/ot-embed.js?embedId=2b0c2f80-276e-4577-b9d6-1bf4938698fa&room=DEFAULT_ROOM&iframe=true"
+             allow="microphone; camera"
+           />
           </Col>
         </Row>
       </Grid>
