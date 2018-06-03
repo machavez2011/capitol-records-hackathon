7fe884ff1f817684e6e0e9e9227f06ff792f2432
Michael Chavez <machavez2011@gmail.com>
2018-06-03 03:13:49 -0700

    Moved iframe to RapBattle component

--- content/src/components/RapBattle.js
+++ content/src/components/RapBattle.js
@@ -2,13 +2,23 @@ import React from "react";
 import RapWords from "./rapWords";
 
 class RapBattle extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <RapWords />
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="row">
+          <div className="col-md-6">
+            <iframe
+              src="https://tokbox.com/embed/embed/ot-embed.js?embedId=2b0c2f80-276e-4577-b9d6-1bf4938698fa&room=DEFAULT_ROOM&iframe=true"
+              allow="microphone; camera"
+            />
+          </div>
+          <div className="col-md-6">
+            <RapWords />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default RapBattle;
