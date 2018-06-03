173aea7c5ce1b66d80d2abd2949ba9ca6329dbbf
Brian Lacey <ss3teddy@gmail.com>
2018-06-03 11:04:19 -0700

    Getting latest.

--- content/src/components/Video.js
+++ content/src/components/Video.js
@@ -1,41 +1,66 @@
 import React from "react";
 import { Grid, Row, Col } from "react-bootstrap";
 import WebCam from "react-webcam";
-import WebCamWrapper from "../containers/WebCamWrapper";
 
 class Video extends React.Component {
   constructor(props) {
     super(props);
+    this.timer = 60;
     this.state = {
-      timer: null
+      timer: this.timer,
+      muted: true
     };
     this.onPlay = this.onPlay.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {}
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({
+      timer: this.state.timer - 1
+    });
+    if (this.state.timer === -1) {
+      clearInterval(this.timerId);
+      this.setState({ timer: this.timer });
+    }
   }
 
   onPlay(e) {
-    debugger;
+    this.timerId = setInterval(() => this.tick(), 1000);
+    this.setState({ muted: false });
   }
 
   render() {
     return (
-      <Grid>
-        <Row>
-          <Col>
-            {/* <iframe
-              src="https://tokbox.com/embed/embed/ot-embed.js?embedId=2b0c2f80-276e-4577-b9d6-1bf4938698fa&room=DEFAULT_ROOM&iframe=true"
-              width="800"
-              height="640"
-              scrolling="no"
-              allow="microphone; camera"
-            /> */}
-            <WebCamWrapper>
-              <div style={{ position: "relative" }}>
-                <WebCam width="800" height="600" muted={true} />
-              </div>
-            </WebCamWrapper>
-          </Col>
-        </Row>
-      </Grid>
+      <div style={{ position: "relative" }}>
+        <div style={{ position: "relative" }}>
+          <WebCam muted={this.state.muted} />
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            top: "1%",
+            left: "1%",
+            color: "white",
+            fontSize: "300%"
+          }}
+        >
+          {this.state.timer}
+        </div>
+        <input
+          type="button"
+          style={{ position: "absolute", bottom: "2%", right: "4%" }}
+          className="btn btn-lg btn-success"
+          value="Start"
+          onClick={this.onPlay}
+          hidden={this.state.timer !== this.timer}
+        />
+      </div>
     );
   }
 }
