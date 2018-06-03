91410366db904eb0b8b7b8a3bbf84b3f8ee1044a
Michael Chavez <machavez2011@gmail.com>
2018-06-03 03:42:56 -0700

    Added users component and nested it withing RapBattle

--- content/src/components/RapBattle.js
+++ content/src/components/RapBattle.js
@@ -1,5 +1,6 @@
 import React from "react";
 import RapWords from "./rapWords";
+import Users from "./Users";
 
 class RapBattle extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class RapBattle extends React.Component {
               src="https://tokbox.com/embed/embed/ot-embed.js?embedId=2b0c2f80-276e-4577-b9d6-1bf4938698fa&room=DEFAULT_ROOM&iframe=true"
               allow="microphone; camera"
             />
+            <Users />
           </div>
           <div className="col-md-6">
             <RapWords />
